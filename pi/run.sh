bba25031e54049ab8b85800eb63f45f4aad92a40
Chris Thach <12981621+cthach@users.noreply.github.com>
2024-03-27 14:14:54 -0400

    Update run.sh

--- pi/run.sh
+++ pi/run.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-set -e
+set -ex
+
+# Switch to parent directory where script is located
+cd "$(dirname "$0")" && cd ..
 
 # Use arecord -D <DEVICE> ... if you need to use a different microphone (list them with arecord -L and prefer plughw: devices). 
 # Use aplay -D <DEVICE> ... if you need to use a different playback device (list them with aplay -L and prefer plughw: devices).
-script/run \
+python3 script/run \
   --name 'satellite1' \
   --uri 'tcp://0.0.0.0:10700' \
   --mic-command 'arecord -r 16000 -c 1 -f S16_LE -t raw' \
